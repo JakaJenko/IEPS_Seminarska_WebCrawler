638a8f22ffed99c2f2bfffc99a92d49fe1e44be2
julijanjug <julijan.jug@gmail.com>
2020-03-05 10:38:54 +0100

    Added PageData info

---
+++ Business/PageData/PageDataBusinessController.py
@@ -0,0 +1,46 @@
+from Business.AbstractDatabaseBusinessController import AbstractDatabaseBusinessController
+from Business.PageData.PageDataInfo import PageDataInfo
+
+
+class PageDataBusinessController(AbstractDatabaseBusinessController):
+    def __init__(self):
+        super().__init__()
+
+    def Select(self):
+        page_data = []
+
+        cur = self.conn.cursor()
+        cur.execute("SELECT id, page_id, data_type_code, data FROM crawldb.page_data")
+
+        for id, page_id, data_type_code, data  in cur.fetchall():
+            page_data.append(PageDataInfo(id, page_id, data_type_code, data ))
+
+        cur.close()
+        return page_data
+
+    def SelectById(self, id):
+        cur = self.conn.cursor()
+        cur.execute("SELECT id, page_id, data_type_code, data FROM crawldb.page_data WHERE id=%s", (id,))
+
+        value = cur.fetchone()[0]
+        page_data_info = PageDataInfo(value.id, value.page_id, value.data_type_code, value.data)
+        cur.close()
+        return page_data_info
+
+    def Insert(self, page_data_info):
+        cur = self.conn.cursor()
+        cur.execute("INSERT INTO crawldb.page_data (page_id, data_type_code, data) VALUES (%s, %s, %s)",
+                    (page_data_info.page_id, page_data_info.data_type_code, page_data_info.data))
+        return True
+
+    def Update(self, page_data_info):
+        try:
+            cur = self.conn.cursor()
+            cur.execute("""UPDATE crawldb.page_data
+                            SET page_id = %s, data_type_code=%s data=%s
+                            WHERE id = %s""",
+                        (page_data_info.page_id, page_data_info.data_type_code, page_data_info.data, page_data_info.id))
+            cur.close()
+            return True
+        except:
+            return False
---
+++ Business/PageData/PageDataInfo.py
@@ -0,0 +1,6 @@
+class PageDataInfo():
+    def __init__(self, id, page_id, data_type_code, data):
+        self.id = id
+        self.page_id = page_id
+        self.data_type_code = data_type_code
+        self.data = data
