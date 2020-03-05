37e03730ee898de9c5854dc10c910c9ea77abfb3
julijanjug <julijan.jug@gmail.com>
2020-03-05 10:12:37 +0100

    Added image class/controller

---
+++ Business/Image/ImageBusinessController.py
@@ -0,0 +1,7 @@
+from Business.AbstractDatabaseBusinessController import AbstractDatabaseBusinessController
+from Business.Image.ImageInfo import ImageInfo
+
+
+class ImageBusinessController(AbstractDatabaseBusinessController):
+    def __init__(self):
+        super().__init__()
---
+++ Business/Image/ImageInfo.py
@@ -0,0 +1,8 @@
+class ImageInfo():
+    def __init__(self, id, page_id, filename, content_type, data, accessed_time):
+        self.id = id
+        self.page_id = page_id
+        self.filename = filename
+        self.content_type = content_type
+        self.data = data
+        self.accessed_time = accessed_time
