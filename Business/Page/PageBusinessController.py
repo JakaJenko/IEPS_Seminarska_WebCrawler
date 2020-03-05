be3c57b88877ef99434d35fd79ffa57576b75425
Jaka Jenko <jaka.jenko@kivi.eu>
2020-03-05 09:14:36 +0100

    Fixes Business Site

--- Business/Page/PageBusinessController.py
+++ Business/Page/PageBusinessController.py
@@ -3,6 +3,9 @@ from Business.Page.PageInfo import PageInfo
 
 
 class PageBusinessController(AbstractDatabaseBusinessController):
+    def __init__(self):
+        super().__init__()
+
     def Select(self):
         page_infos = []
 
@@ -20,25 +23,22 @@ class PageBusinessController(AbstractDatabaseBusinessController):
         page_info = None
 
         cur = self.conn.cursor()
-        cur.execute("SELECT * FROM crawldb.page WHERE id=%s", (id))
+        cur.execute("SELECT * FROM crawldb.page WHERE id=%s", (id,))
 
-        for id, site_id, page_type_code, url, html_content, http_status_code, accessed_time in cur.fetchall():
-            page_info = PageInfo(id, site_id, page_type_code, url, html_content, http_status_code, accessed_time)
+        value = cur.fetchone()[0]
+        page_info = PageInfo(value.id, value.site_id, value.page_type_code, value.url, value.html_content,
+                             value.http_status_code, value.accessed_time)
 
         cur.close()
         return page_info
 
-    '''
+
     def Insert(self, page_info):
         cur = self.conn.cursor()
-        cur.execute("INSERT () INTO crawldb.page)
-
-        for id, site_id, page_type_code, url, html_content, http_status_code, accessed_time in cur.fetchall():
-            page_info = PageInfo(id, site_id, page_type_code, url, html_content, http_status_code, accessed_time)
-
-        cur.close()
-        return page_info
-    '''
+        cur.execute("INSERT INTO crawldb.page VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                    (page_info.id, page_info.site_id, page_info.page_type_code, page_info.url, page_info.html_content,
+                     page_info.http_status_code, page_info.accessed_time))
+        return True
 
     def Update(self, page_info):
         pass
