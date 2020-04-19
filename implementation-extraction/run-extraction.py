c7463a364d780e2f891b452c69449377a81d4f9c
julijanjug <julijan.jug@gmail.com>
2020-04-19 18:47:12 +0200

    run-extraction main

---
+++ implementation-extraction/run-extraction.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    if mode == 'A':  # regular expressions
+        pass
+    elif mode == 'B':  # XPath
+        pass
+    elif mode == 'C':  # adRunner-like implementation
+        pass
