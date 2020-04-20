a4e5d59934a11299040f2f39068f0e39f508cb11
julijanjug <julijan.jug@gmail.com>
2020-04-20 15:52:37 +0200

    Added structure

---
+++ implementation-extraction/RoadRunnerExtraction.py
@@ -0,0 +1,16 @@
+
+class RoadRunnerExtraction:
+
+    def __init__(self, rtvslo_pages=[], overstock_pages=[], mimovrste_pages=[]):
+        self.rtvslo_pages = rtvslo_pages
+        self.overstock_pages = overstock_pages
+        self.mimovrste_pages = mimovrste_pages
+
+    def RtvsloExtraction(self):
+        return
+
+    def OverstockExtraction(self):
+        return
+
+    def MimovrsteExtraction(self):
+        return
--- implementation-extraction/run-extraction.py
+++ implementation-extraction/run-extraction.py
@@ -1,20 +1,26 @@
 import sys
 import requests
+from RegexExtraction import RegexExtraction
+from XPathExtraction import XPathExtraction
+from RoadRunnerExtraction import RoadRunnerExtraction
 
+rtvslo_paths = [
+    '../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html',
+    '../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html']
+overstock_paths = [
+    '../input-extraction/overstock.com/jewelry01.html',
+    '../input-extraction/overstock.com/jewelry02.html']
+mimovrste_paths = []
 
-page_paths = [
-    'overstock.com/jewelry01.html',
-    'overstock.com/jewelry02.html',
-    'rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html',
-    'rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html'
-]
+regexExtractor = RegexExtraction(rtvslo_paths, overstock_paths, mimovrste_paths)
+xpathExtractor = XPathExtraction(rtvslo_paths, overstock_paths, mimovrste_paths)
+roadrunnerExtractor = RoadRunnerExtraction(rtvslo_paths, overstock_paths, mimovrste_paths)
+
+regexExtractor.RtvsloExtraction()
+
 
 if __name__ == "__main__":
-    mode = sys.argv[1]
-    pages_contents = []
-    for page_path in page_paths:
-        pageContent = open('../input-extraction/'+page_path, 'rb').read()
-        pages_contents.append(pageContent)
+    mode = 'A' #sys.argv[1]
 
     if mode == 'A':  # regular expressions
         pass
