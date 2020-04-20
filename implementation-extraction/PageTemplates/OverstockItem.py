c5136b76fa1389e24607c90c51167365835ada18
julijanjug <julijan.jug@gmail.com>
2020-04-20 17:23:15 +0200

    Added page templates

---
+++ implementation-extraction/PageTemplates/OverstockItem.py
@@ -0,0 +1,10 @@
+
+class OverstockItem:
+
+    def __init__(self, Title, Price, ListPrice, Content, Sarving, SavingPercent):
+        self.Title = Title
+        self.Price = Price
+        self.ListPrice = ListPrice
+        self.Content = Content
+        self.Saving = Sarving
+        self.SavingPercent = SavingPercent
---
+++ implementation-extraction/PageTemplates/RtvsloItem.py
@@ -0,0 +1,10 @@
+
+class RtvsloItem:
+
+    def __init__(self, Author, Title, PublishedTime, SubTitle, Lead, Content):
+        self.Author = Author
+        self.Title = Title
+        self.PublishedTime = PublishedTime
+        self.SubTitle = SubTitle
+        self.Lead = Lead
+        self.Content = Content
