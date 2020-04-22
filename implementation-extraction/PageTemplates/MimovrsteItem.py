0e50a2d9e10abad6483aa465af7842143e993ec8
Jaka Jenko <jaka.jenko@kivi.eu>
2020-04-22 15:16:43 +0200

    Classes now have list of item class

--- implementation-extraction/PageTemplates/MimovrsteItem.py
+++ implementation-extraction/PageTemplates/MimovrsteItem.py
@@ -1,9 +1,12 @@
+class MimovrsteItems():
+    # MimovrsteItems is list of MimovrsteItem
+    def __init__(self, MimovrsteItems):
+        self.MimovrsteItems = MimovrsteItems
 
-class MimovrsteItem():
-
-    def __init__(self, Title, Price, ListPrice, Description, Availability):
-        self.Title = Title
-        self.Price = Price
-        self.ListPrice = ListPrice
-        self.Description = Description
-        self.Availability = Availability
+    class MimovrsteItem():
+        def __init__(self, Title, Price, ListPrice, Description, Availability):
+            self.Title = Title
+            self.Price = Price
+            self.ListPrice = ListPrice
+            self.Description = Description
+            self.Availability = Availability
--- implementation-extraction/PageTemplates/OverstockItem.py
+++ implementation-extraction/PageTemplates/OverstockItem.py
@@ -1,10 +1,13 @@
+class OverstockItems():
+    # OverstockItems is list of OverstockItem
+    def __init__(self, OverstockItems):
+        self.OverstockItems = OverstockItems
 
-class OverstockItem():
-
-    def __init__(self, Title, Price, ListPrice, Content, Saving, SavingPercent):
-        self.Title = Title
-        self.Price = Price
-        self.ListPrice = ListPrice
-        self.Content = Content
-        self.Saving = Saving
-        self.SavingPercent = SavingPercent
+    class OverstockItem():
+        def __init__(self, Title, Price, ListPrice, Content, Saving, SavingPercent):
+            self.Title = Title
+            self.Price = Price
+            self.ListPrice = ListPrice
+            self.Content = Content
+            self.Saving = Saving
+            self.SavingPercent = SavingPercent
